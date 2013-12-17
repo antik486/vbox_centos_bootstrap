90372fea5030f7d3a3118eafa5cec648cc743ceb
Anton Ovchar <anton.ovchar@gmail.com>
2013-12-17 20:51:21 +0200

    switch to centos 6.5

--- base.sh
+++ base.sh
@@ -4,8 +4,8 @@
 VBM="/usr/bin/VBoxManage"
 WGET="/usr/local/bin/wget"
 
-ISO_NAME="CentOS-6.4-x86_64-netinstall.iso"
-ISO_URL="http://mirror.yandex.ru/centos/6.4/isos/x86_64/${ISO_NAME}"
+ISO_NAME="CentOS-6.5-x86_64-netinstall.iso"
+ISO_URL="http://mirror.yandex.ru/centos/6.5/isos/x86_64/${ISO_NAME}"
 
 VM="base_test_alpha"
 OSTYPE="RedHat_64"
@@ -14,27 +14,18 @@ VM_FS_SIZE="8192"
 VM_RAM=512
 
 function websrv {
-#	while true;
-#	do 
 		nc -l 3333  < anaconda-ks.cfg; 
-#	done
 }
-
 
 
 $WGET -c ${ISO_URL} -O "$(dirname $0)/${ISO_NAME}"
 #########
 
-
-mkisofs -o  CentOS-6.0-x86_64-netinstall-kickstart.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -R -J -v -T .
-
-
+mkisofs -o  CentOS-6.5-x86_64-netinstall-kickstart.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -R -J -v -T .
 
 $VBM createvm --name "${VM}" --ostype "${OSTYPE}" --register
 
 $VBM createhd --filename "${VM_FS_PATH}/${VM}.vdi" --size "${VM_FS_SIZE}"
-
-
 
 $VBM modifyvm "${VM}" --memory "${VM_RAM}"
 
@@ -43,7 +34,6 @@ $VBM storagectl "${VM}" --name "SATA Controller" --add sata  --controller IntelA
 $VBM storageattach "${VM}" --storagectl "SATA Controller" --port 0  --device 0 --type hdd --medium "${VM_FS_PATH}/${VM}.vdi"
 
 $VBM storageattach "${VM}" --storagectl "SATA Controller" --port 1  --device 0 --type dvddrive --medium "${ISO_NAME}"
-
 
 VirtualBox --startvm "${VM}"
 
